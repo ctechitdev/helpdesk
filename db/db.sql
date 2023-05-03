d30f946b09d85cf033ca8cd32a26268e13235e02
itdevctech <itdevctech@gmail.com>
2023-05-03 16:49:52 +0700

    billynew03052023

--- db/db.sql
+++ db/db.sql
@@ -1,20 +1,77 @@
-create or replace table tbl_user (
-    usid int not null PRIMARY KEY AUTO_INCREMENT,
-    full_name varchar(300),
-    user_name varchar(30), 
-    user_password varchar(30),
-    role_id int,
+
+
+create or replace table tbl_issue_category (
+    isc_id int not null PRIMARY KEY AUTO_INCREMENT,
+    isc_name varchar(50)
+);
+
+create or replace table tbl_issue_type(
+    ist_id int not null PRIMARY KEY AUTO_INCREMENT,
+    isc_id int,
+    ist_name varchar(150)
+);
+
+create or replace table tbl_issue_status (
+    is_id int not null PRIMARY KEY AUTO_INCREMENT,
+    is_name varchar(300)
+);
+
+create or replace table tbl_issue_request(
+    ir_id int not null PRIMARY KEY AUTO_INCREMENT,
+    ist_id int,
+    ir_state int,
+    ir_detail text,
+    reqeust_by int,
+    request_date date,
+    assign_by int,
+    assign_date date,
+    rate_point int
+);
+
+create or replace table tbl_issue_histoty(
+    ih_id int not null PRIMARY KEY AUTO_INCREMENT,
+    ir_id int,
+    ir_state int,
+    ih_detail text,
+    update_by int,
+    update_date date
+);
+
+create or replace table tbl_request_email(
+ re_id int not null PRIMARY KEY AUTO_INCREMENT,
+ user_id int,
+ user_email varchar(100),
+ pass_email varchar(30),
+ date_request date,
+ update_by int,
+ date_update date 
+);
+
+create or replace table tbl_item_data(
+    item_id int not null PRIMARY KEY AUTO_INCREMENT,
+    item_name varchar(300)
+);
+
+create or replace table tbl_request_status(
+    rs_id int not null PRIMARY KEY AUTO_INCREMENT,
+    rs_name varchar(150)
+);
+
+create or replace table tbl_request_use_item(
+    rui_id int not null PRIMARY KEY AUTO_INCREMENT,
+    rui_bill_number varchar(30),
+    rs_id int,
     depart_id int,
-    br_id int,
-    user_status int,
-    add_by int,
-    date_register date
+    request_by int,
+    reqeust_date date
+);
+
+create or replace table tbl_request_use_item_detail(
+    riud_id int not null PRIMARY KEY AUTO_INCREMENT,
+    rui_id int,
+    item_id int,
+    item_value int
 );
 
 
-create table tbl_role_level(
-    rl_id int not null PRIMARY KEY AUTO_INCREMENT,
-    rl_name varchar(90)
-);
 
- 
