a9409b308e7bc689656fbca02285bdd1282bbf2a
ctechitdev <ctech.itdev@gmail.com>
2023-05-08 15:22:03 +0700

    UPDATE

--- query/update-cancel.php
+++ query/update-cancel.php
@@ -5,7 +5,7 @@ include("../setting/conn.php");
  extract($_POST);
 
 
-$delExam = $conn->query(" update tbl_issue_request set ir_state ='1'WHERE ir_id = '$ir_id'  ");
+$delExam = $conn->query(" update tbl_issue_request set ir_state ='1', assign_date ='NULL',assign_by='NULL' WHERE ir_id = '$ir_id'  ");
 if($delExam)
 {
 	$res = array("res" => "success");
