74921095ddc63e0e2c9932f465aa231e8fad6745
ctechitdev <ctech.itdev@gmail.com>
2023-05-10 16:31:56 +0700

    bee T10 4:30

--- query/dropdown/item_list_branch.php
+++ query/dropdown/item_list_branch.php
@@ -3,12 +3,7 @@
 include("../../setting/checksession.php");
 include("../../setting/conn.php");
 
-$stmt1 = $conn->prepare(" 
-select a.item_id ,item_name
-from tbl_item_price a
-left join tbl_item_data b on a.item_id = b.item_id  
-where br_id = '$br_id' and status_item_price = '1' 
-");
+$stmt1 = $conn->prepare("SELECT * FROM tbl_item_data");
 $stmt1->execute();
 
 $data = $stmt1->fetchAll();
